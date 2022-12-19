5b9f9c37b67e37d2d7093fb8fc55505ae0fea302
SangaviArjunan <120551046+SangaviArjunan@users.noreply.github.com>
2022-12-19 12:27:39 +0530

    Update ArrayIntersection.java

--- src/task2/ArrayIntersection.java
+++ src/task2/ArrayIntersection.java
@@ -1,20 +1,23 @@
 package task2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * @author SArjunan
- *
+ * 
+ *         To find the Array Intersection Run this class and follow the
+ *         instructions given in console
+ * 
  */
 public class ArrayIntersection {
 
 	/**
-	 * 
+	 * this method takes 2 array inputs and returns common numbers from both arrays
+	 * in an ArrayList
 	 */
-	static int[] out = null;
-	static ArrayList<Integer> al = new ArrayList<>();
-
-	public static void intersect(int[] a1, int[] a2) {
+	public static ArrayList<Integer> intersect(int[] a1, int[] a2) {
+		ArrayList<Integer> al = new ArrayList<>();
 		for (int a = 0; a < a1.length; a++) {
 			for (int b = 0; b < a2.length; b++) {
 				if (a1[a] == a2[b]) {
@@ -24,15 +27,31 @@ public class ArrayIntersection {
 				}
 			}
 		}
+		return al;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] a1 = { 9, 4, 5, 9 };
-		int[] a2 = { 8, 7, 5, 9, 1, 2, 9, 9, 5 };
-		intersect(a1, a2);
+		ArrayList<Integer> al = new ArrayList<>();
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter number of elements in first array:");
+			int a = scanner.nextInt();
+			int x[] = new int[a];
+			System.out.println("Enter first array elements:");
+			for (int i = 0; i < a; i++) {
+				x[i] = scanner.nextInt();
+			}
+			System.out.println("Enter number of elements in second array:");
+			a = scanner.nextInt();
+			int y[] = new int[a];
+			System.out.println("Enter second array elements:");
+			for (int i = 0; i < a; i++) {
+				y[i] = scanner.nextInt();
+			}
+			al = intersect(x, y);
+		}
 		System.out.println(al);
 	}
 
